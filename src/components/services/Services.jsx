06b1b931b5ccde9f3f777a42843b6087b8bc7206
Peru PC <paulocondori1@gmail.com>
2022-08-25 09:07:31 -0400

    services part is completed but new text is still missing

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,22 +1,152 @@
 import React from 'react'
 import './services.scss'
+import { BsBookmarkStar } from 'react-icons/bs'
 
 const services = () => {
-return (
+  return (
     <section id='services'>
       <h5>What I Offer</h5>
       <h2>Services</h2>
 
 
-      <div className="container services_container">
+      <div className="container service_container">
         <article className='service'>
           <div className="service_head">
             <h3>UI/UX Design</h3>
           </div>
 
-<ul className="service_list">
-  <li></li>
-</ul>
+          <ul className="service_list">
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit amet, consecm repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum ndaeanimi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem sentium, necessiimi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum doloue deserui, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolortemporibus praeriam repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor siibus prais aperiam repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+          </ul>
+
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit amet,  optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit amet, aperiam repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit aibus praesentium, necessit</p>  
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sitbus praesentium, necessitat animi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit amet, coaperiam repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sitmporiaperiam repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+          </ul>
+
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sibus prae repellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor nrepellat animi, sed voluptate optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit amet,  optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit ame optio.
+              </p>
+            </li>
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor, repudiandae temporoptio.
+              </p>
+            </li>
+
+
+            <li>
+              <BsBookmarkStar className='service_listIcons' />
+              <p>Lorem ipsum dolor sit ametreuptate optio.
+              </p>
+            </li>
+
+          </ul>
 
         </article>
       </div>
