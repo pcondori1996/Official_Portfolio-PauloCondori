12bd9432a9a8a38a67f3e750db2117314b0bbc9d
Peru PC <paulocondori1@gmail.com>
2022-08-25 15:17:40 -0400

    portfolio is done. used a map function to display all projects and made it easy to update in the future

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,66 @@
+
 import React from 'react'
-import './portfolio.css'
+import './portfolio.scss'
+import project1 from '../../assets/Project1pic.jpg'
+import project2 from '../../assets/Wookiee-Warriors.png'
+import project3 from '../../assets/samaritan.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: project1,
+    title: 'Touch Grass',
+    github: 'https://ashley1thompson.github.io/touch-grass-app-national-park-finder/',
+    demo: 'https://github.com/Ashley1Thompson/touch-grass-app-national-park-finder'
+  },
+  {
+    id: 2,
+    image: project2,
+    title: 'Wookies Gold',
+    github: 'https://github.com/pcondori1996/WookiesGold',
+    demo: ''
+  },
+  {
+    id: 3,
+    image: project3,
+    title: 'Superhero (in progress)',
+    github: '',
+    demo: ''
+  }
+];
+
 
 const Portfolio = () => {
   return (
-   <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio_item">
+                <div className="portfolio_itemImage">
+                  <img src={image} alt={title} className='actualImage' />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio_itemHolder'>
+                  <a href={github} targte='_blank' className='btn'>Github Repo</a>
+                  <a href={demo} taget='_blank' className='btn'>Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+
+
+
+
+
+      </div>
+
+    </section>
   )
 }
 
