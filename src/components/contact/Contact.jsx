217bdff6a5e677b5d6ee18d8dbf15156749fd48d
Peru PC <paulocondori1@gmail.com>
2022-08-26 18:15:23 -0400

    footer is done, deploying now

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.scss'
-import {MdOutlineMail} from 'react-icons/md'
-import {BsFacebook} from 'react-icons/bs'
-import {BsWhatsapp} from 'react-icons/bs'
+import { MdOutlineMail } from 'react-icons/md'
+import { BsFacebook } from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-return (
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_94zqgei', 'template_8ayitsv', form.current, 't1ZuUBV7GhSDeIqHo')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
+
+  return (
     <section id='contact'>
       <h5>Get In Touch</h5>
       <h2>Contact me</h2>
@@ -13,28 +27,38 @@ return (
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineMail />
-<h4>Email</h4>
-<h5>paulocondori1@gmail.com</h5>
-<a href="mailto:paulocondori1@gmail.com">Send a message</a>
+            <MdOutlineMail className='contactOptionIcons
+'/>
+            <h4>Email</h4>
+            <h5>paulocondori1@gmail.com</h5>
+            <a href="mailto:paulocondori1@gmail.com">Send a message</a>
           </article>
 
           <article className="contact_option">
-            <BsFacebook />
-<h4>Facebook</h4>
-<h5>My profile</h5>
-<a href="https://m.me/ernest,achiever">Send a message</a>
+            <BsFacebook className='contactOptionIcons
+'/>
+            <h4>Facebook</h4>
+            <h5>My profile</h5>
+            <a href="https://m.me/ernest,achiever">Send a message</a>
           </article>
 
           <article className="contact_option">
-            <BsWhatsapp />
-<h4>Whatsapp</h4>
-<h5>To my account</h5>
-<a href="https://api.whatsapp.com/send?phone+">Send a message</a>
+            <BsWhatsapp className='contactOptionIcons
+'/>
+            <h4>Whatsapp</h4>
+            <h5>To my account</h5>
+            <a href="https://api.whatsapp.com/send?phone=6785385206">Send a message</a>
           </article>
         </div>
+
+
         {/* END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message'></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import './footer.css'
+import './footer.scss'
+import { FiFacebook } from 'react-icons/fi'
+import { AiOutlineLinkedin} from 'react-icons/ai'
+import {BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer '>EGATOR</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.facebook.com/Paulo.Condori/"><FiFacebook /> </a>
+        <a href="https://www.linkedin.com/in/paulosergio1996/"><AiOutlineLinkedin /></a>
+        <a href="https://github.com/pcondori1996"><BsGithub /></a>
+    </div>
+
+    <div className="footer_copyright">
+      <small>&copy; Paulo Sergio Condori Pinedo </small>
+    </div>
+    </footer >
   )
 }
 
